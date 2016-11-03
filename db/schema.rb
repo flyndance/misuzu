9458c0e40607f9b6b311ab82de39e06c5fbe7d35
anhmt <maitrunganh212@gmail.com>
2016-11-03 10:36:27 +0700

    fix bashomaster

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,22 @@ ActiveRecord::Schema.define(version: 20161102054832) do
 
   add_index "JOBマスタ", ["job番号"], name: "index_JOBマスタ_on_job番号", unique: true, using: :btree
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
   create_table "events", force: :cascade do |t|
     t.string   "社員番号"
     t.string   "開始"
@@ -173,7 +189,6 @@ ActiveRecord::Schema.define(version: 20161102054832) do
     t.string   "備考"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "color"
   end
 
   create_table "分類マスタ", id: false, force: :cascade do |t|
@@ -204,7 +219,6 @@ ActiveRecord::Schema.define(version: 20161102054832) do
     t.string   "備考"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "color"
   end
 
   create_table "回覧詳細", force: :cascade do |t|
