75016d578da522fc3809c71e0731fd025895792d
anhmt <maitrunganh212@gmail.com>
2016-12-02 15:08:24 +0700

    replace function getUrlVars to application.js

--- app/assets/javascripts/time_line_view.js
+++ app/assets/javascripts/time_line_view.js
@@ -302,11 +302,11 @@ $(function(){
 
 });
 
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
-    function(m,key,value) {
-      vars[key] = value;
-    });
-    return vars;
-  }
+// function getUrlVars() {
+//     var vars = {};
+//     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+//     function(m,key,value) {
+//       vars[key] = value;
+//     });
+//     return vars;
+//   }
